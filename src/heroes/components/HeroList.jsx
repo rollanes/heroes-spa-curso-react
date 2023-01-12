92ba77a6a6765abfe40f53e3bfaa7772bc7f3983
Rocio Llanes <rollanes13@gmail.com>
2023-01-12 18:07:24 -0300

    se crean tarjetas de heroes

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,14 @@
+import { HeroCard } from "./HeroCard";
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className="row row-cols-1 row-cols-md-3 g-3">
       {heroes.map( hero => (
-        <li key={ hero.id }>{ hero.superhero }</li>
+        <HeroCard key={ hero.id } { ...hero }/>
       ))}
-    </ul>
+    </div>
   );
 };
