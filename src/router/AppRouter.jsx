a53acd202faf2d17a8e72a7bec52e08a09cdb4b2
Rocio Llanes <rollanes13@gmail.com>
2023-01-12 16:26:36 -0300

    se crean archivos de barril para imports

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { LoginPage } from "../auth/LoginPage";
+
+import { MarvelPage, DcPage } from "../heroes";
+import { LoginPage } from "../auth";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DcPage />} />
