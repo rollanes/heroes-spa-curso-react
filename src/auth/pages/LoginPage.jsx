ebd195dec9db2dd96bd4184c5d20147fd372c3df
Rocio Llanes <rollanes13@gmail.com>
2023-01-12 17:03:40 -0300

    se agrega navegacion con useNavigate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useNavigate } from "react-router"
+
 
 export const LoginPage = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/', {
+      replace: true
+    });
+  }
   return (
-    <h1>LoginPage</h1>
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr/>
+
+      <button onClick={handleLogin} className='btn btn-outline-warning'>
+        Login
+      </button>
+    </div>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/login', {
+      replace: true
+    });
+  }
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -25,7 +34,7 @@ export const Navbar = () => {
             Rosie
           </span>
 
-          <button className="nav-item nav-link btn btn-warning text-black">
+          <button onClick={handleLogout} className="nav-item nav-link btn btn-warning text-black">
             Logout
           </button>
         </ul>
