f49135ead5eb30552fc809f2dba2d6650da63fd1
Rocio Llanes <rollanes13@gmail.com>
2023-01-12 19:31:34 -0300

    se diseña SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { HeroCard } from "../components";
 
 export const SearchPage = () => {
   return (
-    <h1>Search</h1>
-  )
-}
+    <>
+      <h1>Search page</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Search</h4>
+          <hr />
+
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <button className="btn btn-outline-warning mt-1">Search!</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-warning">Search a hero...</div>
+          <div className="alert alert-dark">We couldn't find your hero</div>
+
+          {/*<HeroCard />*/}
+        </div>
+      </div>
+    </>
+  );
+};
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -25,6 +25,10 @@ export const Navbar = () => {
           <NavLink className={({ isActive }) => `nav-item nav-link ${ isActive ? 'active' : '' }`} to="/dc">
             DC
           </NavLink>
+
+          <NavLink className={({ isActive }) => `nav-item nav-link ${ isActive ? 'active' : '' }`} to="/search">
+            Search
+          </NavLink>
         </div>
       </div>
 
